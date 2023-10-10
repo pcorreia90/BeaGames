1f7452f0b191ba11a5e7147a4550a74d01eae894
Santas <pcorreia90@gmail.com>
2023-10-10 18:19:37 +0100

    finished logic that check if previous and next numbers are correct and shows the correct message

--- files/index.js
+++ files/index.js
@@ -4,35 +4,45 @@ const confirmNumBtn = document.getElementById('confirmBtn');
 const previousNumInput = document.getElementById('previousNum');
 const nextNumInput = document.getElementById('nextNum');
 const resultMessage = document.getElementById('resultMsg')
-
-let hasNewNum = false;
+const happyFace = "\uD83D\uDE04";
+const sadFace = "\uD83D\uDE22";
 
 randomNumElement.innerHTML = 0;
+confirmNumBtn.disabled = true;
 
 getNewNumBtn.addEventListener('click', function (){
     generateRandomNumber(100);
+    previousNumInput.value = ""
+    nextNumInput.value = ""
+    resultMessage.innerHTML = ""
+    getNewNumBtn.disabled = true
+    confirmNumBtn.disabled = false
 });
 
 confirmNumBtn.addEventListener('click', function (){
-    console.log(previousNumInput.value)
-    if(previousNumInput.value == "" || nextNumInput.value == ""){
-        console.log("maminhas")
-        resultMessage.innerHTML ="Que pena! Está errado.";
+    let currentNum = parseInt(randomNumElement.innerHTML)
+    console.log(currentNum)
+    let previousNum = parseInt(previousNumInput.value)
+    console.log(previousNum)
+    let nextNum = parseInt(nextNumInput.value)
+    console.log(nextNum)
+
+    if(previousNum == null || nextNum == null){
+        resultMessage.innerHTML ="Tens de preencher os dois números.";
         return;
     }
-    resultMessage.innerHTML = "Boa! Está correcto."
+
+    if(previousNum === currentNum - 1 && nextNum === currentNum + 1) {
+        resultMessage.innerHTML = "Boa! Está correcto." + happyFace;
+        getNewNumBtn.disabled = false;
+        return;
+    }
+
+    resultMessage.innerHTML = "Que pena! Está errado, volta a tentar." + sadFace;
+    
 })
 
 
 function generateRandomNumber(maxInt) {
     randomNumElement.innerHTML = Math.floor(Math.random() * maxInt) + 1
 };
-
-
-
-
-
-
-
-
-
